e3a609d5a230062e3d535ac1f6eba074eeed0889
Lin <lin.guzman@tecsup.edu.pe>
2024-10-16 15:40:41 -0500

    Initial Commit

---
+++ GuzmanSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuzmanSnapchat
+//
+//  Created by Lin Abigail Guzman Gutierrez on 16/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
